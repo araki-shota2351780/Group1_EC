8902258c2471a4d899b1c88eed22231df72e0ff4
Reeze0811 <2301212@s.asojuku.ac.jp>
2024-12-05 12:17:11 +0900

    最新版
    最新版チェックアウト画面、コンビニ払い画面

--- purchase/Conveni.php
+++ purchase/Conveni.php
@@ -1,64 +1,90 @@
+<?php
+session_start();
+
+// フォームデータを受け取る
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // 他のデータも受け取る
+    $name1 = $_POST['name1'];
+    $name2 = $_POST['name2'];
+    $zip_code = $_POST['zip_code'];
+    $prefecture = $_POST['prefecture'];
+    $city = $_POST['city'];
+    $house_number = $_POST['house_number'] ?? '';
+    $building_name = $_POST['building_name'] ?? '';
+    $payment_method = $_POST['payment_method'];
+
+    // 一時的にセッションにデータを保存
+    $_SESSION['name1'] = $name1;
+    $_SESSION['name2'] = $name2;
+    $_SESSION['zip_code'] = $zip_code;
+    $_SESSION['prefecture'] = $prefecture;
+    $_SESSION['city'] = $city;
+    $_SESSION['house_number'] = $house_number;
+    $_SESSION['building_name'] = $building_name;
+    $_SESSION['payment_method'] = $payment_method;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>コンビニ払い画面</title>
-<link rel="stylesheet" href="./css/checkout.css">
-<link rel="stylesheet" href="./css/conveni.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>コンビニ払い画面</title>
+    <link rel="stylesheet" href="./checkout/css/checkout.css">
+    <link rel="stylesheet" href="./checkout/css/conveni.css">
 </head>
 <body>
-<div class="container">
-    <!-- × ボタンはコンテナ内に配置 -->
-    <span class="close-button" onclick="location.href='cart.html'">×</span>
-    <h2><em>CHECKOUT</em></h2>
-    ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣
-        </script>
-    <h4>コンビニ画面</h4>
-    <form action="" method="post">
-    <ul class="convenience-list">
+    <div class="container">
+        <!-- × ボタンはコンテナ内に配置 -->
+        <span class="close-button" onclick="location.href='checkout.php'">×</span>
+        <h2><em>CHECKOUT</em></h2>
+        ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣
+        <h4>コンビニ画面</h4>
+        <form action="payment.php" method="post">
+        <ul class="convenience-list">
         <li class="convenience-item">
             <input type="radio" name="conveni[]" value="ファミマ" id="familymart">
             <label for="familymart">
-                <img src="./../image/ファミリーマート.png"  height=20rem>
+                <img src="http://aso2301212.mods.jp/EC/checkout/img/ファミリーマート.png"  height=20rem>
                 ファミリーマート
             </label>
         </li>
         <li class="convenience-item">
             <input type="radio" name="conveni[]" value="ローソン" id="lawson">
             <label for="lawson">
-                <img src="./../image/ローソン.png"  height=20rem>
+                <img src="http://aso2301212.mods.jp/EC/checkout/img/ローソン.png"  height=20rem>
                 ローソン
             </label>
         </li>
         <li class="convenience-item">
             <input type="radio" name="conveni[]" value="デイリーヤマザキ" id="dailyyamazaki">
             <label for="dailyyamazaki">
-                <img src="./../image/デイリーヤマザキ.png"  height=20rem>
+                <img src="http://aso2301212.mods.jp/EC/checkout/img/デイリーヤマザキ.png"  height=20rem>
                 デイリーヤマザキ
             </label>
         </li>
         <li class="convenience-item">
             <input type="radio" name="conveni[]" value="ミニストップ" id="ministop">
             <label for="ministop">
-                <img src="./../image/ミニストップ.png"  height=20rem>
+                <img src="http://aso2301212.mods.jp/EC/checkout/img/ミニストップ.png"  height=20rem>
                 ミニストップ
             </label>
         </li>
         <li class="convenience-item">
             <input type="radio" name="conveni[]" value="セブンイレブン">
             <label for="seveneleven">
-                <img src="./../image/セブンイレブン.png"  height=20rem>
+                <img src="http://aso2301212.mods.jp/EC/checkout/img/セブンイレブン.png"  height=20rem>
                 セブンイレブン
             </label>
         </li>
-    <h5>注文を確定されてから24時間以内お支払いがされなかった場合自動的に注文はキャンセルされます</h5>
+        <h5>注文を確定されてから24時間以内お支払いがされなかった場合自動的に注文はキャンセルされます</h5>
 
-        <h4>受け取り方法</h4>
-        <input type="radio" name="receive[]" value="置き">置き配（配達BOX）<br>
-        <input type="radio" name="receive[]" value="対面">対面で受け取り<br>
-        <button type="button">最終確認へ進む</button>
-    </form>
-</div>
+            <h4>受け取り方法</h4>
+            <input type="radio" name="receive_method" value="置き" required>置き配（配達BOX）<br>
+            <input type="radio" name="receive_method" value="対面"required>対面で受け取り<br>
+            <input type="submit" value="最終確認へ進む">
+        </form>
+    </div>
 </body>
 </html>
--- purchase/checkout.php
+++ purchase/checkout.php
@@ -14,40 +14,39 @@
     ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣
     <script src="./js/checkout.js"></script>
     <h4>配達希望日</h4>
-    <form  id="paymentForm" method="post">
-        <input type="date" id="deli_date" name="deli_date"  required><br>
+    <form method="post" onsubmit="redirectToPage(); return false;">
+        <input type="date" id="deli_date" name="deli_date" required><br>
         <h4>配達を希望する時間帯</h4>
-        <select name="deli_time[]" >
-            <option value="all">いつでも</option>
+        <select name="deli_time[]">
+            <option value="all">指定なし</option>
             <option value="morning">9:00～12:00</option>
             <option value="noon">12:00～15:00</option>
             <option value="evening">15:00～18:00</option>
             <option value="night">18:00～21:00</option>
         </select>
         <h4>配達</h4>
-            <input type="text" id="name" name="address" placeholder="姓" required><!--id適当-->
-            <input type="text" id="name" name="address" placeholder="名" required><br>
-            <input type="text" id="address" name="address" placeholder="郵便番号" required><br>
-            <input type="text" id="address" name="address" placeholder="都道府県" required><br>
-            <input type="text" id="address" name="address" placeholder="市区町村" required><br>
-            <input type="text" id="address" name="address" placeholder="番地" required><br>
-            <input type="text" id="address" name="address" placeholder="建物名、部屋番号など" ><br><!--必須項目ではない-->
+        <label for="name1"></label>
+        <input type="text" id="name1" name="name1" placeholder="姓" required>
+        <label for="name2"></label>
+        <input type="text" id="name2" name="name2" placeholder="名" required><br>
+        <label for="zip_code"></label>
+        <input type="text" id="zip_code" name="zip_code" placeholder="郵便番号" required><br>
+        <label for="prefecture"></label>
+        <input type="text" id="prefecture" name="prefecture" placeholder="都道府県" required><br>
+        <label for="city"></label>
+        <input type="text" id="city" name="city" placeholder="市区町村" required><br>
+        <label for="house_number"></label>
+        <input type="text" id="house_nuber" name="house_number" placeholder="番地" required><br>
+        <label for="building_name"></label>
+        <input type="text" id="building_name" name="building_name" placeholder="建物名、部屋番号など"><br><!--必須項目ではない-->
         <h4>支払い</h4>
-        <label>
-            <input type="radio" name="payment[]" value="クレジットカード"> クレジットカード
-        </label>
-        <br>
-        <label>
-            <input type="radio" name="payment[]" value="代金引換"> 代金引換(手数料:￥330 税込み)
-        </label>
-        <br>
-        <label>
-            <input type="radio" name="payment[]" value="コンビニ"> コンビニ(手数料:￥330 税込み)
-        </label>
-        <br><br>
-        <p>※支払いが確認された後発注されます</p>
-        <button type="button" onclick="redirectToPage()">続ける</button>
-    </form>
+        <input type="radio"   id="payment_method" name="payment_method" value="クレジットカード">クレジットカード<br>
+        <input type="radio"  id="payment_method" name="payment_method" value="代金引換">代金引換(手数料:￥330 税込み)<br>
+        <input type="radio"  id="payment_method" name="payment_method" value="コンビニ">コンビニ(手数料:￥330 税込み)<br><br>
+        <p>支払いが確認された後発注されます</p>
+        <input type="submit" value="続ける"  >
+        </form>
+
 </div>
 </body>
 </html>
